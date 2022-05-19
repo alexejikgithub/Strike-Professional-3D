7f0c30d72ce0c0505a4b6e553737553d09064e37
alexejikgithub <62622414+alexejikgithub@users.noreply.github.com>
2022-05-19 21:06:28 +0300

    Set WayPoint movement

---
+++ Assets/Scriots/CharacterAnimationController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(AgentLinkMover))]
+public class CharacterAnimationController : MonoBehaviour
+{
+
+	[SerializeField] private Animator _animator;
+
+	private NavMeshAgent _agent;
+	private AgentLinkMover _linkMover;
+
+	private const string _isRunning = "IsRunning";
+	private const string _jumping = "Jumping";
+	private const string _landed = "Landed";
+
+	private void Awake()
+	{
+		_agent = GetComponent<NavMeshAgent>();
+		_linkMover = GetComponent<AgentLinkMover>();
+
+		_linkMover.OnLinkStart += JumpStart;
+		_linkMover.OnLinkEnd += JumpEnd;
+	}
+
+	private void Start()
+	{
+
+	}
+
+	private void Update()
+	{
+		SetRunningAnimation();
+	}
+
+	private void SetRunningAnimation()
+	{
+		_animator.SetBool(_isRunning, _agent.velocity.magnitude > 0.5f);
+	}
+
+	private void JumpStart()
+	{
+		_animator.SetTrigger(_jumping);
+	}
+	private void JumpEnd()
+	{
+		_animator.SetTrigger(_landed);
+	}
+
+	private void OnDestroy()
+	{
+		_linkMover.OnLinkStart -= JumpStart;
+		_linkMover.OnLinkEnd -= JumpEnd;
+	}
+
+
+}
--- Assets/Scriots/WayPointComponent.cs
+++ Assets/Scriots/WayPointComponent.cs
@@ -6,9 +6,15 @@ public class WayPointComponent : MonoBehaviour
 {
 	[SerializeField] Color _gizmosColor;
 
-	
+	[SerializeField] private bool _isWPClear;
+
+	public bool IsWPClear => _isWPClear;
+
+
 	private void OnDrawGizmos()
 	{
+
+		
 		Gizmos.color = _gizmosColor;
 
 		Gizmos.DrawSphere(transform.position, 0.5f);
